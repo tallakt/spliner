efb315e7a44bfe81f6c950795c03830cbbc299c5
Al Chou <HotFusionMan+GitHub@Gmail.com>
2013-06-16 23:09:38 -0700

    Optimized for speed and accuracy.

--- lib/spliner/spliner_section.rb
+++ lib/spliner/spliner_section.rb
@@ -24,10 +24,10 @@ module Spliner
 
     def calculate_a_k
       if @x.size > 1
-        inv_diff = @x.each_cons(2).map {|x1, x2| 1 / (x2 - x1) }
+        inv_diff = @x.each_cons(2).map {|x1, x2| 1.0 / (x2 - x1) }
         a_diag = 2.0 * Matrix::diagonal(*vector_helper(inv_diff))
         a_non_diag = Matrix::build(@x.size) do |row, col|
-          if row == col+ 1
+          if row == col + 1
             inv_diff[col]
           elsif col == row + 1
             inv_diff[row]
@@ -41,11 +41,12 @@ module Spliner
         tmp = @x.zip(@y).each_cons(2).map do |p1, p2|
           x1, y1 = p1
           x2, y2 = p2
-          3.0 * (y2 - y1) / (x2 - x1) ** 2.0
+          delta_x = (x2 - x1)
+          3.0 * (y2 - y1) / (delta_x * delta_x)
         end
         b = vector_helper(tmp)
 
-        @k = a.inv * b
+        @k = a.lup_decomposition.solve( b )
       else
         @k = Vector[0.0]
       end
@@ -56,13 +57,17 @@ module Spliner
     def get(v)
       i = @x_pairs.find_index {|pair| pair.member? v }
       if i
-        dx = @x[i + 1] - @x[i]
-        dy = @y[i + 1] - @y[i]
-        t = (v - @x[i]) / dx
-        a = @k[i] * dx - dy
-        b = -(@k[i + 1] * dx - dy)
+        x_pair = @x_pairs[i]
+        x_min = x_pair.min
+        dx = x_pair.max - x_min
+        y_max = @y[i + 1]
+        y_min = @y[i]
+        dy = y_max - y_min
+        t = (v - x_min) / dx
+        a =      @k[i] * dx - dy
+        b = -@k[i + 1] * dx + dy
         one_minus_t = 1 - t
-        t * @y[i + 1] + one_minus_t * (@y[i] + t * (a * one_minus_t + b * t))
+        t * y_max + one_minus_t * ( y_min + t * ( a * one_minus_t + b * t ) )
       elsif @x.size == 1 && @x.first == v
         @y.first
       else
